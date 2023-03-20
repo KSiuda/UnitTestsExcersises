42d15cc9a20b4cfba8287dd7c1a74e6311f0d1b7
KSiuda <konrad.siuda@gmail.com>
2023-03-20 13:20:51 +0100

    Change example for students

---
+++ UnitTestExample/Garbage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCity
+{
+	internal class Garbage
+	{
+		public GarbageType GarbageType { get; private set; }
+		public Garbage() { }
+
+		public void Remove()
+		{
+
+		}
+	}
+
+	public enum GarbageType
+	{
+		BETON,
+		RURA,
+		PAPIER,
+		SZMATKA,
+		MJENSKO
+	}
+}
--- UnitTestExample/SmartWasteBin.cs
+++ UnitTestExample/SmartWasteBin.cs
@@ -1,4 +1,6 @@
-﻿namespace UnitTestExample
+﻿using SmartCity;
+
+namespace UnitTestExample
 {
 	public class SmartWasteBin
 	{
@@ -6,26 +8,56 @@
 
 		public string ErrorMessage { get; set; }
 
-		public SmartWasteBin(bool compactor)
-		{
-			Compactor = compactor;
-		}
+		private List<Garbage> Garbages { get; set; }
+	
 
-		public bool Compact(string garbageType)
-		{
-			if (!Compactor) return false;
-
-			if (garbageType.ToUpper() == "BETON")
-			{
-				return false;
-			}
-			else if (garbageType.ToUpper() == "RURA")
-			{
-				ErrorMessage = "Rury to nie tutaj";
-				return false;
-			}
-
-			return true;
-		}
+	public SmartWasteBin(bool compactor)
+	{
+		Compactor = compactor;
 	}
+
+	public bool Compact(GarbageType garbageType)
+	{
+		if (!Compactor) return false;
+
+		if (garbageType == GarbageType.BETON)
+		{
+			return false;
+		}
+		else if (garbageType == GarbageType.RURA)
+		{
+			ErrorMessage = "Rury to nie tutaj";
+			return false;
+		}
+
+		return true;
+	}
+
+	public void Empty()
+	{
+		if (Garbages.Any())
+		{
+			Garbages.ForEach(x => { x.Remove();});
+		}
+
+		if (Garbages.Any(x => x.GarbageType is GarbageType.BETON or GarbageType.RURA))
+		{
+			return;
+		}
+
+		Garbages.Clear();
+	}
+
+	public string Clean()
+	{
+		var message = "CLEANED";
+		if (Garbages.Any())
+		{
+			message = "CAN'T CLEAN";
+		}
+		return message;
+	}
+}
+
+	
 }
